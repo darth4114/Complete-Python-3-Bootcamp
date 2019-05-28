6079b5a21eb3e8a346d265b63c50b4be9555545c
darth4114 <darthmaul4114@gmail.com>
2019-05-27 21:47:19 -0700

    complexity

--- 02-Python Statements/TestGame.py
+++ 02-Python Statements/TestGame.py
@@ -1,10 +1,35 @@
 from random import randint
 
-user_num = int(input('Input a number!'))
+print('Wecome to the Guessing Game!')
+print('See how close you can get to the number!')
 
-#user_num = 13
+ans = randint(1,101)
+guesses = [0]
 
-if user_num == randint(0,3):
-    print('You have guessed the right number!')
-else:
-    print('Sorry you did not guess correctly :(')
+while True:
+    guess = int(input('PIck your number!\n'))
+
+    #check for valid answer
+    if guess < 1 or guess > 100:
+        print('OUT OF BOUNDS! Please try again: ')
+        break
+
+    #check for correct answer
+    if guess == ans:
+        print(f'CONGRATULATIONS, YOU GUESSED IT IN {len(guesses)} GUESSES!')
+        break
+
+    #add to list
+    guesses.append(guess)
+
+    #check for difference
+    if guesses[-2]:
+        if abs(ans-guess) < abs(ans-guesses[-2]):
+            print('WARMER!')
+        else:
+            print('COLDER!')
+    else:
+        if abs(ans-guess) <= 10:
+            print('WARM!')
+        else:
+            print('COLD!')
